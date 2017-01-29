f106d8fb10cf535560d0012fa1608815dec6dc43
Silva01 <silva20102@gmail.com>
2017-01-29 19:17:21 -0200

    Adicionado service de manipulação de arquivos

--- app/js/controllers/inicio.controller.js
+++ app/js/controllers/inicio.controller.js
@@ -1,3 +1,25 @@
-app.controller('home', function($scope) {
+app.controller('inicio', function($scope, Arquivo) {
   $scope.titulo = "Olá Pagina Home";
+
+  $scope.criar = function() {
+    var json = {
+      time: [{
+        nome: "Daniel",
+        idade: 27
+      },{
+        nome: "luffy",
+        idade: 26
+      }]
+    };
+
+    Arquivo.salvar(json);
+  }
+
+  $scope.ler = function() {
+    $scope.dados = Arquivo.ler();    
+  }
+
+  $scope.excluir = function() {
+    Arquivo.excluir();
+  }
 });
--- app/js/rotas/rotas.js
+++ app/js/rotas/rotas.js
@@ -2,7 +2,7 @@ app.config(function($routeProvider, $locationProvider, $httpProvider) {
   $routeProvider
   .when('/', {
     templateUrl: 'views/inicio.html',
-    controller: ""
+    controller: "inicio"
   })
   .otherwise({
     redirectTo: '/'
