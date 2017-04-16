8c8d940c4189c449228e882fd903f5bd4bdc6f8b
Daniel Silva <silva20102@gmail.com>
2017-04-15 21:29:32 -0300

    Adicionado nova estrutura de gravação de dados e calculos dos minutos

--- app/js/pomodoro.js
+++ app/js/pomodoro.js
@@ -77,9 +77,10 @@ var timer = undefined;
 var breakTime = false;
 var dados = {
   "minutos": 0,
-  "segundos": 0,
   "data": null
 };
+
+var minutosCrecente = 0;
 
 //start timer
 startButton.addEventListener('click', function () {
@@ -89,8 +90,7 @@ startButton.addEventListener('click', function () {
   spinner.classList.toggle("spinning");
   statusDisplay.innerHTML = "Pomodoro Iniciado!";
 
-  dados.minutos = sessionTimer;
-  dados.segundos = 0;
+  dados.minutos = convertToMinutoMillissegundos(sessionTimer);
   dados.data = new Date();
 
   minutes = sessionTimer - 1;
@@ -114,18 +114,20 @@ startButton.addEventListener('click', function () {
         seconds = 60;
         elapsedPercent = 0;
         statusDisplay.innerHTML = "Pausa";
+        minutosCrecente = 0;
       } else {
         breakTime = false;
         minutes = sessionTimer - 1;
         seconds = 60;
         elapsedPercent = 0;
         statusDisplay.innerHTML = "Pomodoro Iniciado!";
-        console.log("work time");
+        minutosCrecente = 0;
       }
     }
     if (seconds === 0) {
       minutes--;
       seconds = 60;
+      minutosCrecente++;
     }
   };
   function progress() {
@@ -149,10 +151,10 @@ resetButton.addEventListener('click', function () {
   clearInterval(timer);
 
   if (breakTime === false) {
-      dados.minutos = minutes;
-      dados.segundos = seconds;
+      dados.minutos = convertToMinutoMillissegundos(sumMinutesSeconds(minutosCrecente, seconds));
       dados.data = new Date();
       gravar(dados);
+      console.log(minutosCrecente);
   } else {
       breakTime = false;
       minutes = sessionTimer - 1;
