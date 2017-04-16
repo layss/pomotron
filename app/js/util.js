d654985c589cf81c7d45c22ef887b207f423a5e6
Daniel Silva <silva20102@gmail.com>
2017-04-15 21:03:46 -0300

    Implementado utilidades de tempo

--- app/js/util.js
+++ app/js/util.js
@@ -1,3 +1,17 @@
 /**
  * Created by DanielSilva on 15/04/17.
  */
+
+(function () {
+  this.convertToMinutoMillissegundos = function (minuto) {
+      return minuto * 60000;
+  };
+
+  this.sumMinutesSeconds = function (minutes, seconds) {
+      return minutes + convertToSecondMinutos(seconds);
+  };
+
+  var convertToSecondMinutos = function (segundos) {
+      return segundos / 60;
+  };
+}())
